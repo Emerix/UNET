e45db575629c1f8453aa2751e390beb9ef1511b4
Emerix <paul@lubos.com>
2015-06-08 15:02:23 +0200

    Initial commit.
    Just fooling around with UNET. Far from perfect.

---
+++ Assets/InputController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+public class InputController : NetworkBehaviour {
+
+	[Command]
+	public void CmdJump()
+	{   
+		Debug.Log("running on server");
+		GetComponent<Rigidbody>().AddForce(new Vector3(0,5,0),ForceMode.Impulse);
+	}
+	
+	[ClientCallback]
+	void Update()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			RaycastHit h;
+			Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast(r,out h))
+			{
+				if(h.transform == transform)
+				{
+					CmdJump();
+				}
+			}
+		}
+		
+	}
+}
